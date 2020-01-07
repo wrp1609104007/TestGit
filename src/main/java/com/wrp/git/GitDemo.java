9b7d67e5320be8c765c1375d937ead1a5900d6e9
wrp_glo <helloWrp_glo@wrp.com>
2020-01-07 12:49:59 +0800

    test hot_fix

--- src/main/java/com/wrp/git/GitDemo.java
+++ src/main/java/com/wrp/git/GitDemo.java
@@ -3,6 +3,6 @@ package com.wrp.git;
 public class GitDemo {
    public static void main(String[] args) {
 	System.out.println("haha....");
-	System.out.println("hot_fix");
+	System.out.println("hot_fix....");
 }
 }
