63f57297a2b1c0ca25516069739e9fb3c0b4474a
wrp_glo <helloWrp_glo@wrp.com>
2020-01-07 12:38:00 +0800

    test

--- src/main/java/com/wrp/git/GitDemo.java
+++ src/main/java/com/wrp/git/GitDemo.java
@@ -2,8 +2,6 @@ package com.wrp.git;
 
 public class GitDemo {
    public static void main(String[] args) {
-	System.out.println("xixi..");
 	System.out.println("haha....");
-	System.out.println("liguangfu");
 }
 }
